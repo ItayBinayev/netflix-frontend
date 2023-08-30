1c2a86e7d500d8e5b0bc47d0f729df1618367646
Itay <itaybinayev@gmail.com>
2023-08-30 16:32:45 +0300

    changed content card

--- src/Components/ContentCard/ContentCard.jsx
+++ src/Components/ContentCard/ContentCard.jsx
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { BsFillPlayFill } from 'react-icons/bs'
-import ReactPlayer from 'react-player';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BsFillPlayFill } from "react-icons/bs";
+import { GoMute , GoUnmute} from 'react-icons/go'
+import ReactPlayer from "react-player/youtube";
 
-const ContentCard = ({data, isFirstInGroup, isLastInGroup }) => {
-    const [showVideo, setShowVideo] = useState(false);
-    const isMobile = window.innerWidth <= 640;
-  
-    const handleMouseEnter = () => setShowVideo(true);
-    const handleMouseLeave = () => setShowVideo(false);
-  
-    const [rating, setRating] = useState(0);
-    const navigate = useNavigate()
+const ContentCard = ({ data, isFirstInGroup, isLastInGroup }) => {
+  const [showVideo, setShowVideo] = useState(false);
+  const [muted, setMuted] = useState(true);
+  const isMobile = window.innerWidth <= 640;
+  const navigate = useNavigate();
 
-    const playHandler = async () => {
-        const idData = await data._id;
-        navigate(`/content/${idData}`)
-    }
+  const handleMouseEnter = () => setShowVideo(true);
+  const handleMouseLeave = () => {
+    setShowVideo(false);
+    setPrevImgOpacity(100);
+  };
 
-    useEffect(() => {
-        // Use your rating calculation logic here, for example:
-        const calculatedRating = 90 + Math.floor(Math.random() * 10); // Random rating between 0 and 100
-        setRating(calculatedRating);
-      }, []);
-    
-      let hoverIndent = "";
+  const [prevImgOpacity, setPrevImgOpacity] = useState(100);
+
+  const hideImageOnPlayHandler = () => {
+    setPrevImgOpacity(0);
+  };
+
+  const [rating, setRating] = useState(0); // Initialize with 0 or a default value
+  // Calculate the rating when the component mounts
+  useEffect(() => {
+    // Use your rating calculation logic here, for example:
+    const calculatedRating = 90 + Math.floor(Math.random() * 10); // Random rating between 0 and 100
+    setRating(calculatedRating);
+  }, []);
+
+  let hoverIndent = "";
 
   if (isFirstInGroup || isLastInGroup) {
     hoverIndent = isFirstInGroup
@@ -32,11 +38,9 @@ const ContentCard = ({data, isFirstInGroup, isLastInGroup }) => {
       : "group-hover:translate-x-[-3vw]";
   }
 
-  
-
   return (
     <div
-      className="group bg-zinc-900 relative h-[10vw]"
+      className="group bg-zinc-900 relative w-full h-[10vw]"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
@@ -46,29 +50,50 @@ const ContentCard = ({data, isFirstInGroup, isLastInGroup }) => {
         alt={data.title}
       />
       <div
-        className={`absolute opacity-0 top-0 transition duration-200 z-10 shadow-md invisible sm:visible delay-300 w-full scale-0 group-hover:scale-125 group-hover:-translate-y-[6vw] ${hoverIndent} group-hover:opacity-100`}
+        className={`absolute opacity-0 top-0 transition duration-400 z-10 shadow-md invisible sm:visible delay-300 w-full h-full scale-0 group-hover:scale-125 group-hover:-translate-y-[6vw] ${hoverIndent} group-hover:opacity-100`}
       >
-        <div className="bg-black w-full h-[12vw] pointer-events-none">
+        <div className="w-full h-full">
+          <img
+            className={`absolute inset-0 object-cover h-full w-full ${
+              showVideo ? "transition" : ""
+            } opacity-${prevImgOpacity}`}
+            src={data.imgThumb}
+            alt=""
+          />
           {showVideo && !isMobile && (
-            <ReactPlayer
-              url={data.movie}
-              controls={false}
-              disablePictureInPicture={true}
-              playing
-              width="100%"
-              height="100%"
-            />
+            <>
+              <ReactPlayer 
+                url={data.movie}
+                controls={false}
+                disablePictureInPicture={true}
+                playing
+                width="100%"
+                height="100%"
+                onPlay={hideImageOnPlayHandler}
+                muted={muted}
+                volume={0.3}
+                loop={true}
+              />
+              <button 
+                className="absolute flex items-center justify-center left-0 bottom-0 cursor-pointer w-6 h-6 lg:w-8 lg:h-8 bg-white rounded-full transition hover:bg-neutral-300 text-black lg:ml-4 ml-2 mb-2"
+                onClick={() => setMuted(!muted)}
+              >
+                {muted ? (<GoMute size={25}/>) : (<GoUnmute size={25}/>)}
+              </button>
+            </>
           )}
+          
         </div>
-        <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
-          <div className="flex flex-row items-center gap-3">
-            <div
-              className="cursor-pointer my-1 w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300 text-black"
-              onClick={playHandler}
-            >
-              <BsFillPlayFill size={30} />
-            </div>
+        <div className="z-10 flex flex-col bg-zinc-800 gap-1 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
+          <div className="flex flex-row items-center space-x-3">
+            
           </div>
+            <div
+              className="cursor-pointer w-6 h-6 lg:w-8 lg:h-8 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300 text-black gap-3"
+              onClick={() => navigate("/content/" + data._id)}
+            >
+              <BsFillPlayFill size={25} />
+            </div>
 
           <div className="flex flex-row gap-2 items-center">
             <p className="text-green-400 font-semibold">{rating}% Match</p>
@@ -80,7 +105,7 @@ const ContentCard = ({data, isFirstInGroup, isLastInGroup }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContentCard
+export default ContentCard;
